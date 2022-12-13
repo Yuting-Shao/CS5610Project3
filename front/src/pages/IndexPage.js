edd95e3204e43759d2fb74151f860b5a22c7caf7
AndrewXu23 <105327407+AndrewXu23@users.noreply.github.com>
2022-12-12 22:51:39 -0800

    font change

--- front/src/pages/IndexPage.js
+++ front/src/pages/IndexPage.js
@@ -29,29 +29,7 @@ function IndexPage() {
           </button>
         </div>
         <div>
-          <p className="yellowtext">
-            In year 2077, human has explored all planets in Oort cloud. They
-            owned the technology to manage a global ecosystem of a just explored
-            planet. United Nation wonder if we can simulate the operation result
-            of exploiting a given planet. Therefore, they hired 79 years old
-            computer science masters Zihan and Yuting from Northeastern
-            University to design a simulator web with react, mongoDB and all
-            required functions from professor John.
-          </p>
-        </div>
-        <div>
           <img src={MyImage2} alt="wave1" class="bottom-img" />
-        </div>
-        <div>
-          <p className="yellowtext">
-            In year 2077, human has explored all planets in Oort cloud. They
-            owned the technology to manage a global ecosystem of a just explored
-            planet. United Nation wonder if we can simulate the operation result
-            of exploiting a given planet. Therefore, they hired 79 years old
-            computer science masters Zihan and Yuting from Northeastern
-            University to design a simulator web with react, mongoDB and all
-            required functions from professor John.
-          </p>
         </div>
       </div>
       <div>
